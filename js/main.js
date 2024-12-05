9285e86fa7854d36fa454f1363d831faa6905949
salamiiwastaken <gays1328@gmail.com>
2024-12-05 12:43:18 -0600

    Update main.js
    added the basic variables and functions

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@
 
 
 /*----- state variables -----*/
-
-
+var wealth = 5;
+var happiness = 5;
+var health = 5;
+var parenthealth = 5;
+var years = 0;
 
 /*----- cached elements -----*/
 
@@ -15,3 +18,13 @@
 
 
 /*----- functions -----*/
+
+function updateScreen () {
+	if(wealth <= 0 || happiness <= 0 || health <= 0 || parenthealth <= 0){
+
+		//this will be the code to handle a premature game over 
+	}else{
+
+		//this will update the stats, and move to the next problem / question
+	}
+});

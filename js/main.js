b76f4c8d140be8d9d86d44bad89ed10e2679a97e
salamiiwastaken <gays1328@gmail.com>
2024-12-05 13:42:33 -0600

    Update main.js
    added more constants, worked on adding some of the solutions to the problems, and added the premature game-over text. still have to wait for the CSS to be finished before i can actually fill in the element constants

--- js/main.js
+++ js/main.js
@@ -1,30 +1,95 @@
 /*----- constants -----*/
-
-
+const problem = getElementById();
+const wealthStat = getElementById();
+const happinessStat = getElementById();
+const healthStat = getElementById();
+const parentHealthStat = getElementById();
+const intellectStat = getElementById();
+const optionbutton1 = getElementById();
+const optionbutton2 = getElementById();
+const optionbutton3 = getElementById();
+const minigameButton = getElementById();
+const majorChoice1 = [Year6A, Year6B, Year6C];
+const majorChoice2 = [Year9A, Year9B, Year9C];
+const majorChoice3 = [Year13A, Year13B, Year13C];
+const majorChoice4 = [Year16A, Year16B, Year16C];
+const majorChoice5 = [Year18A, Year18B, Year18C];
 
 /*----- state variables -----*/
 var wealth = 5;
 var happiness = 5;
 var health = 5;
-var parenthealth = 5;
+var parentHealth = 5;
+var intellect = 5;
 var years = 1;
+var eventCount = 1;
+
+var modifiers = 0;
+
 
 /*----- cached elements -----*/
 
 
 
 /*----- event listeners -----*/
-
+optionButton1.addEventListener("click", function(){
+	if(years == 1 && eventCount == 1){
+		years = years + 1;
+		var bottleChance = randomNumber(1,2)
+		if (randomNumber == 1){
+			problem.textContent=("Your baby eventually came around. He just needed a little more time.");
+			health = health + 1;
+			eventCount = eventCount + 1;
+			updateScreen();
+		}else{
+			problem.textContent=("After a long while of waiting, you decide to switch your baby over anyway.")
+			parentHealth = parentHealth - 1;
+			wealth = wealth - 1;
+			eventCount = eventCount + 1;
+			updateScreen();
+		}
+	}
+	if(years == 1 && eventCount == 2){
+		problem.textContent=("While your sleep may suffer, your baby went to bed faster with you there.");
+        parentHealth = parentHealth - 1;
+        health = health + 1;
+        eventCount = eventCount + 1;
+        updateScreen();
+	}
+    if(years == 1 && eventCount == 3){
+        problem.textContent=("While your child is falling slightly behind, you set up your future for you and your family");
+        wealth = wealth + 2;
+        happiness = happiness - 2;
+        eventCount = 1;
+        updateScreen();
+    }
+});
+optionButton2.addEventListener("click", function(){
+	
+});
+optionButton3.addEventListener("click", function(){
+	
+});
 
 
 /*----- functions -----*/
 
 function updateScreen () {
-	if(wealth <= 0 || happiness <= 0 || health <= 0 || parenthealth <= 0){
-
+	if(wealth <= 0 || happiness <= 0 || health <= 0 || parentHealth <= 0 || intellect <= 0){
+		problem.textContent=("You failed at raising your child. Your child made it to " + years + "years old.");
 		//this will be the code to handle a premature game over 
 	}else{
-
-		//this will update the stats, and move to the next problem / question
+		wealthStat.textContent=("Wealth ( $ ) = " + wealth);
+		happinessStat.textContent=("Happiness ( :D ) = " + happiness);
+		healthStat.textContent=("Health ( <3 ) = " + health);
+		parentHealthStat.textContent=("Parent Wellbeing ( <> ) = " + parentHealth);
+		intellectStat.textContent=("Intellect (IQ) = " + intellect);
+		//this will update the stats ^^
 	}
-});
+};
+
+function option1Selected (){
+	if(majorChoice1[Year6A] == true){
+
+    }
+};

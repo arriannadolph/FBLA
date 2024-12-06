10dc2956bc84f1c565bcd48830cb3916828919da
salamiiwastaken <gays1328@gmail.com>
2024-12-06 13:21:28 -0600

    Update main.js
    added year 3 choices

--- js/main.js
+++ js/main.js
@@ -24,7 +24,9 @@ var intellect = 5;
 var years = 1;
 var eventCount = 1;
 
-var modifiers = 0;
+var majorChoice2Modifiers = 0;
+var majorChoice3Modifiers = 0;
+var MajorChoice5Modifiers = 0;
 
 
 /*----- cached elements -----*/
@@ -33,56 +35,146 @@ var modifiers = 0;
 
 /*----- event listeners -----*/
 optionButton1.addEventListener("click", function(){
-if(years == 1){
+	if(years == 1){
         if(eventCount == 1){
 		    var bottleChance = randomNumber(1,2)
-		    if (randomNumber == 1){
-			    problem.textContent=("Your baby eventually came around. He just needed a little more time.");
+            // makes it a 50% chance on whether or not the child needs to breastfeed
+		    if (bottleChance == 1){
+			    problem.textContent=("Your child eventually came around. He just needed a little more time.");
 			    health = health + 1;
 			    eventCount = eventCount + 1;
 			    updateScreen();
 		    }else{
-			    problem.textContent=("After a long while of waiting, you decide to switch your baby over anyway.")
+			    problem.textContent=("After a long while of waiting, you decide to switch your child over anyway.")
 			    parentHealth = parentHealth - 1;
 			    wealth = wealth - 1;
 			    eventCount = eventCount + 1;
 			    updateScreen();
 		    }
 	    }else if(eventCount == 2){
-		    problem.textContent=("While your sleep may suffer, your baby went to bed faster with you there.");
-            	parentHealth = parentHealth - 1;
-            	health = health + 1;
-            	eventCount = eventCount + 1;
-            	updateScreen();
-	    }else{
-            	problem.textContent=("While your child is falling slightly behind, you set up your future for you and your family");
-            	wealth = wealth + 2;
-            	happiness = happiness - 2;
-            	eventCount = 1;
-            	updateScreen();
-    	}
-}
-});
-optionButton2.addEventListener("click", function(){
-	// there are only two options for the 3 events on year 1, so i will put the second choice into option 3 for aesthetic purposes
-});
-optionButton3.addEventListener("click", function(){
-if(years == 1){
-if(eventCount == 1){
-        var bottleChance = randomNumber(1,2);
-        if(randomNumber == 1){
-        	problem.textContent=("Your child just needed more time. Making the switch wasn't necessary, and your baby actually does better breastfeeding.")
-        	wealth = wealth - 1
-        	eventCount = eventCount + 1;
-        	updateScreen();
-        }else{
-            problem.textContent=("Your baby responded much better to bottle-feeding.")
+		    problem.textContent=("While your sleep may suffer, your child went to bed faster with you there.");
+            parentHealth = parentHealth - 1;
             health = health + 1;
             eventCount = eventCount + 1;
             updateScreen();
+	    }else{
+            problem.textContent=("While your child is falling slightly behind, you set up your future for you and your family");
+            wealth = wealth + 2;
+            happiness = happiness - 2;
+            eventCount = 1;
+            years = 2;
+            updateScreen();
+        }
+    }else if(years == 2){
+        if(eventCount == 1){
+            problem.textContent=("After taking your child to a pediatrician, you discovered that your child's slight delay to walking is completely normal, and not cause for concern.");
+            if(parentHealth > 5){
+                parentHealth = 5;
+            }
+            eventCount = eventCount + 1;
+            updateScreen();
+        }else{
+            problem.textContent=("You decide to punish your child with a punishment that makes them think about what they did. (This will affect the game later.)");
+            majorChoice2Modifiers = majorChoice2Modifiers + 1;
+            majorChoice3Modifiers = majorChoice3Modifiers + 1;
+            MajorChoice5Modifiers = MajorChoice5Modifiers + 1;
+            eventCount = 1;
+            years = 3;
+            updateScreen();
+        }
+    }else if(years == 3){
+        if(eventCount == 1){
+            problem.textContent=("The pediatrician says that the speech delay is temporary, and that you should try encouraging your child to speak, and speak or read to them.");
+            wealth = wealth - 1;
+            intellect = intellect + 1;
+            eventCount = eventCount + 1;
+            updateScreen();
+        }else if(eventCount == 2){
+            problem.textContent=("You take your child to the birthday party. While you're there, your friend talks to you about their child's educational tablet, and tells you to consider buying one.");
+            happiness = happiness + 1;
+            intellect = intellect + 1;
+            eventCount = eventCount + 1;
+            updateScreen();
+        }else{
+            problem.textContent=("You buy your child the tablet, and see it do wonders")
+            wealth = wealth - 1;
+            intellect = intellect + 1;
+            eventCount = 1;
+            years = 4;
         }
     }
-}
+});
+
+optionButton2.addEventListener("click", function(){
+	// there are only two options for the 3 events on year 1, so i will put the second choice into option 3 for aesthetic purposes
+    if(years == 1){
+        problem.style.display='none';
+    }else if(years == 2){
+        if(eventCount == 1){
+            problem.style.display='none';
+        }else{
+            problem.textContent=("You decide not to punish your child at all. (This will affect the game later.)");
+            majorChoice2Modifiers = majorChoice2Modifiers + 1;
+            majorChoice3Modifiers = majorChoice3Modifiers + 1;
+            MajorChoice5Modifiers = MajorChoice5Modifiers + 1;
+            happiness = happiness + 1;
+            intellect = intellect - 1;
+            eventCount = eventCount + 1;
+            updateScreen();
+        }
+    }else if(years == 3){
+        problem.style.display='none';
+    }
+});
+
+optionButton3.addEventListener("click", function(){
+    if(years == 1){
+        if(eventCount == 1){
+            var bottleChance = randomNumber(1,2);
+            // the aforementioned 50% chance of needing the bottle
+            if(bottleChance == 1){
+                problem.textContent=("Your child just needed more time. Making the switch wasn't necessary, and your child actually does better breastfeeding.")
+                wealth = wealth - 1
+                eventCount = eventCount + 1;
+                updateScreen();
+            }else{
+                problem.textContent=("Your child responded much better to bottle-feeding than breastfeeding.")
+                health = health + 1;
+                eventCount = eventCount + 1;
+                updateScreen();
+            }
+        }
+    }else if(years == 2){
+        if(eventCount == 1){
+            problem.textContent=("You decide not to take your child to the pediatrician, but you still worry about your child.");
+            parentHealth = parentHealth - 1;
+            eventCount = eventCount + 1;
+            updateScreen();
+        }else{
+            problem.textContent=("You decide to give your child a physical punishment. (This will affect the game later.)");
+            majorChoice2Modifiers = majorChoice2Modifiers - 2;
+            majorChoice3Modifiers = majorChoice3Modifiers - 2;
+            MajorChoice5Modifiers = MajorChoice5Modifiers - 2;
+            health = health - 1;
+            intellect = intellect - 1;
+            happiness = happiness - 1;
+            eventCount = eventCount + 1;
+            updateScreen();
+        }
+    }else if(years == 3){
+        if(eventCount == 1){
+            problem.textContent=("You decide against it. You're sure that your child will come around eventually");
+            intellect = intellect - 1;
+            eventCount = eventCount + 1;
+            updateScreen();
+        }else{
+            problem.textContent=("You decide against taking your child to the birthday party.");
+            happiness = happiness - 1;
+            eventCount = 1
+            years = 4;
+            updateScreen();
+        }
+    }
 });
 
 
@@ -101,21 +193,3 @@ function updateScreen () {
 		//this will update the stats ^^
 	}
 };
-
-function option1Selected (){
-	if(majorChoice1[Year6A] == true){
-
-    }
-};
-
-function navigate(page){
-	if (page ==='play'){
-		window.location.href='q1y1.html';
-	} else if (page === 'leaderboard'){
-		window.location.href='leaderboard.html';
-	} else if (page === 'options'){
-		window.location.href='options.html';
-	} else if (page === 'settings'){
-		window.location.href='settings.html';
-	}
-}
